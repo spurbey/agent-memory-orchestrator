4b9992b4d2b4e3754ce81fe14ecec8144b6fb9a0
spurbey <spurbey@users.noreply.github.com>
2026-06-01 04:45:30 +0545

    refactor(application): own session graph write

--- src/agent_memory_orchestrator/application/pipeline/__init__.py
+++ src/agent_memory_orchestrator/application/pipeline/__init__.py
@@ -9,15 +9,27 @@ from .job_runner import path_hash
 from .job_runner import require_complete_production_marker
 from .job_runner import stage_config_hash
 from .job_runner import stage_config_payload
+from .graph_writer import CompactKuzuWriteResult
+from .graph_writer import CompactSessionGraph
+from .graph_writer import build_compact_session_graph
+from .graph_writer import write_compact_session_graph
+from .promotion import CuratedGraphBuild
+from .promotion import build_curated_session_graph
 
 __all__ = [
+    "CompactKuzuWriteResult",
+    "CompactSessionGraph",
+    "CuratedGraphBuild",
     "PendingModel",
     "ProductionSessionJobRunner",
     "StageFailed",
     "StageResult",
+    "build_compact_session_graph",
+    "build_curated_session_graph",
     "file_sha256",
     "path_hash",
     "require_complete_production_marker",
     "stage_config_hash",
     "stage_config_payload",
+    "write_compact_session_graph",
 ]
